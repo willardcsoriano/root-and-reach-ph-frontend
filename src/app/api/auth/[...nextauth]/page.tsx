0ab0f45db513cd8ca2b84abd1ad71f1df5658c7e
willardcsoriano <124260532+willardcsoriano@users.noreply.github.com>
2025-07-06 07:46:07 +0800

    removed page.tsx

--- src/app/api/auth/[...nextauth]/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/api/auth/[...nextauth]/page.tsx
-
-// This forces the route segment to use the Node.js runtime
-export const runtime = 'nodejs'; 
-
-// This is a dummy component that will never be rendered
-export default function Page() {
-  return <h1>This page should not be rendered.</h1>;
-}
